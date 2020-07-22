d3db5073211ba9641a73901cb64e31cc442c923f
Darshan <dhabaledarshan@gmail.com>
2020-07-22 20:03:45 +0530

    List HTML added

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,7 +4,7 @@ from .views import RoomListView, BookingList, BookingView, RoomDetailView
 app_name = 'hotel'
 
 urlpatterns = [
-    path('room_list/', RoomListView.as_view(), name='RoomList'),
+    path('room_list/', RoomListView, name='RoomList'),
     path('booking_list/', BookingList.as_view(), name='BookingList'),
     path('book/', BookingView.as_view(), name='BookingView'),
     path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
--- hotel/views.py
+++ hotel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView, FormView, View
+from django.urls import reverse
 from .models import Room, Booking
 from .forms import AvailabilityForm
 from hotel.booking_functions.availability import check_availability
@@ -7,8 +8,22 @@ from hotel.booking_functions.availability import check_availability
 # Create your views here.
 
 
-class RoomListView(ListView):
-    model = Room
+def RoomListView(request):
+    room = Room.objects.all()[0]
+    room_categories = dict(room.ROOM_CATEGORIES)
+    room_values = room_categories.values()
+    room_list = []
+
+    for room_category in room_categories:
+        room = room_categories.get(room_category)
+        room_url = reverse('hotel:RoomDetailView', kwargs={
+                           'category': room_category})
+
+        room_list.append((room, room_url))
+    context = {
+        "room_list": room_list,
+    }
+    return render(request, 'room_list_view.html', context)
 
 
 class BookingList(ListView):
@@ -17,6 +32,7 @@ class BookingList(ListView):
 
 class RoomDetailView(View):
     def get(self, request, *args, **kwargs):
+        print(self.request.user)
         category = self.kwargs.get('category', None)
         form = AvailabilityForm()
         room_list = Room.objects.filter(category=category)
