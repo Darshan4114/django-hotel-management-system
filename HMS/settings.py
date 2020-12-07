eda5101f3067e37cb742806235b8e199a8e67608
Darshan <dhabaledarshan@gmail.com>
2020-12-07 20:09:28 +0530

    changed debug settings to env vars

--- HMS/settings.py
+++ HMS/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG') or True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
