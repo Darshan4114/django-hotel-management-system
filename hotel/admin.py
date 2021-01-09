a7254f43bcc5ed1966fc34725df9a211f3a39d22
Darshan <dhabaledarshan@gmail.com>
2021-01-09 22:13:45 +0530

    😃 added Person model to admin.py

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Room, Booking, RoomCategory
+from .models import Room, Booking, RoomCategory, Person
 
 # Register your models here.
 admin.site.register(RoomCategory)
 admin.site.register(Room)
 admin.site.register(Booking)
+admin.site.register(Person)
